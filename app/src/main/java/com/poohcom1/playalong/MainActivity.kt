017d0c3c3ab9aed5945b5297e7cbb6a392c49f0c
poohcom1 <pooh677@gmail.com>
2023-04-24 22:17:03 +0700

    Added widgets

--- app/src/main/java/com/poohcom1/playalong/MainActivity.kt
+++ app/src/main/java/com/poohcom1/playalong/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.poohcom1.playalong
 
+import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.yausername.youtubedl_android.YoutubeDL
 import com.yausername.youtubedl_android.YoutubeDLException
 import com.yausername.youtubedl_android.YoutubeDLRequest
+import com.yausername.youtubedl_android.mapper.VideoInfo
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,18 +33,19 @@ class MainActivity : AppCompatActivity() {
             Log.e("YoutubeDL", e.toString())
         }
 
-        val urlDebugText: TextView = findViewById(R.id.urlText)
+        val videoView: VideoView = findViewById(R.id.videoView)
+
         val urlInput: EditText = findViewById(R.id.urlInput)
         val urlSubmitButton: Button = findViewById(R.id.urlSubmitButton)
 
-        var updated = false
-
         urlSubmitButton.setOnClickListener {
-            val request = YoutubeDLRequest(urlInput.text.toString())
-            request.addOption("-f", "best");
-            val streamInfo = YoutubeDL.getInstance().getInfo(request)
+            val getUrlRequest = YoutubeDLRequest(urlInput.text.toString())
+            getUrlRequest.addOption("-f", "best")
 
-            urlDebugText.text = streamInfo.url
+            val streamInfo: VideoInfo = YoutubeDL.getInstance().getInfo(getUrlRequest)
+            Log.d("YoutubeDL", streamInfo.url)
+            videoView.setVideoURI(Uri.parse(streamInfo.url))
+            videoView.start()
         }
     }
 }
